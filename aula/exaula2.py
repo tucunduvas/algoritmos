6361158c7bcdbf7b17bcc9a1488b70b6454b4d35
gabriela <gabriela.tucunduva@estudante.ifms.edu.br>
2025-04-03 21:30:35 -0400

    testeeee

--- aula/exaula2.py
+++ aula/exaula2.py
@@ -16,3 +16,24 @@ elif  dia==7:
     print("É sábado!")
 else:
     print("Número fora da range.")
+    
+dia = int(input("Digite o dia da semana em 1 a 7: "))
+
+if dia==1:
+    print("É domingo!")
+elif  dia==2:
+    print("É segunda!")
+elif  dia==3:
+    print("É terça!")
+elif  dia==4:
+    print("É quarta!")
+elif  dia==5:
+    print("É quinta!")
+elif  dia==6:
+    print("É sexta!")
+elif  dia==7:
+    print("É sábado!")
+else:
+    print("Número fora da range.")
+    
+    
